25e65d8115aaf3b5fcbcfc893a59df3a6d9c389a
Chase Arline <chasearline.jobs@gmail.com>
2021-02-25 19:08:38 -0800

    data logging sd test

---
+++ Firmware/Main/DataLogging.ino
@@ -0,0 +1,39 @@
+const int SD_CS = 15;
+
+#include "DataLogging.h"
+
+SdFat sd;
+SdFile myFile;
+
+void testSD {
+    
+  // Initialize SdFat or print a detailed error message and halt
+  // Use half speed like the native library.
+  // change to SPI_FULL_SPEED for more performance.
+  if (!sd.begin(SD_CS, SPI_HALF_SPEED)) sd.initErrorHalt();
+
+  // open the file for write at end like the Native SD library
+  if (!myFile.open("test.txt", O_RDWR | O_CREAT | O_AT_END)) {
+    sd.errorHalt("opening test.txt for write failed");
+  }
+  
+  // if the file opened okay, write to it:
+  Serial.print("Writing to test.txt...");
+  myFile.println("testing 1, 2, 3.");
+
+  // close the file:
+  myFile.close();
+  Serial.println("done.");
+
+  // re-open the file for reading:
+  if (!myFile.open("test.txt", O_READ)) {
+    sd.errorHalt("opening test.txt for read failed");
+  }
+  Serial.println("test.txt:");
+
+  // read from the file until there's nothing else in it:
+  int data;
+  while ((data = myFile.read()) >= 0) Serial.write(data);
+  // close the file:
+  myFile.close();
+}
