e619302f39dc0cd0a365f80a0c8366b5289c31e4
Rouen de la O <rouendelao@gmail.com>
2021-04-22 14:58:01 -0700

    update to method closeContactor()

--- Firmware/Main/Precharge.h
+++ Firmware/Main/Precharge.h
@@ -9,10 +9,9 @@
 #define CLOSE_CONTACTOR_BUTTON 501 // digital pin for closing the contactor (usable after precharging)
 #define CONTACTOR_PRECHARGED_LED 400 // digital pin for LED that illuminates after precharge complete
 #define CONTACTOR_CLOSED_LED 401 // digital pin for LED that illuminates when the contactor is closed 
-#define NUMBER_OF_LTCS 25 // THIS NEEDS TO BE CHANGED TO OUR ACTUAL NUMBER OF LTCs
-#define THROTTLE_TEMP_MAX 60 // THIS ALSO MAY NEED TO BE CHANGED
-#define MOTORCONTROLLER_TEMP_MAX 60 // THIS ALSO MAY NEED TO BE CHANGED
-#define MOTOR_TEMP_MAX 60 // THIS ALSO MAY NEED TO BE CHANGED
+#define NUMBER_OF_LTCS 2 // THIS NEEDS TO BE CHANGED TO OUR ACTUAL NUMBER OF LTCs
+#define MOTORCONTROLLER_TEMP_MAX 65 // THIS ALSO MAY NEED TO BE CHANGED
+#define MOTOR_TEMP_MAX 80 
 
 extern volatile signed char preChargeFlag; 
 enum PC_STATE { PC_START, PC_OPEN , PC_CLOSE, PC_JUST_CLOSED };
