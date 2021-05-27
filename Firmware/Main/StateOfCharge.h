8078bf85f570c638061c1f63045af40c26a2323e
Rouen de la O <rouendelao@gmail.com>
2021-05-27 02:10:27 -0700

    SOC functions written

--- Firmware/Main/StateOfCharge.h
+++ Firmware/Main/StateOfCharge.h
@@ -1,13 +1,23 @@
-
 #ifndef SOC_H_
 #define SOC_H_
 
-enum SOC_STATE { SOC_START, SOC_CHARGING, SOC_DISCHARGING };
+enum SOC_STATE { SOC_START, SOC_CHARGING, SOC_DISCHARGING, SOC_IDLE };
 
 typedef struct SOCTaskData {
   SOC_STATE* SOC_State;
   float* stateOfCharge;
   int* stateOfChargeMemoryAddress;
+  float* dischargingCurrent;
+  float* chargingCurrent;
+  float* batteryCapacity;
 };
+
+#define DISCHARGING_CURRENT_THRESHOLD 9000
+#define CHARGING_CURRENT_THRESHOLD 9000
+#define SOC_TIME_INTERVAL 9000 // seconds between calls to the SOC state machine
+//
+void SOC_FSMTransitionActions (SOCTaskData socData);
+//void SOC_FSMStateActions (SOCTaskData socData);
+//float adjustSOC (SOCTaskData socData, float current);
 
 #endif
--- Firmware/Main/StateOfCharge.ino
+++ Firmware/Main/StateOfCharge.ino
@@ -1,41 +1,47 @@
 #include "StateOfCharge.h"
 
+float adjustSOC(SOCTaskData socData, float current) {
+  return (*socData.stateOfCharge) * current * SOC_TIME_INTERVAL / (*socData.batteryCapacity) * 1.0;
+}
+
+// this function must be called AFTER SOC_FSMStateActions
 void SOC_FSMTransitionActions (SOCTaskData socData){
   switch (*(socData.SOC_State)) { // transitions
     case SOC_START:
-
       break;
-    case PC_OPEN:
-
+    case SOC_CHARGING:
       break;
-    case PC_CLOSE:
-
-      break;
-    case PC_JUST_CLOSED:
-
+    case SOC_DISCHARGING:
       break;
     default:
       *socData.SOC_State = SOC_START;
       break;
   } // transitions
+  // transition conditions are independant of the current State
+  if (*socData.chargingCurrent > CHARGING_CURRENT_THRESHOLD) {
+    *socData.SOC_State = SOC_CHARGING; 
+  } else if (*socData.dischargingCurrent > DISCHARGING_CURRENT_THRESHOLD) {
+    *socData.SOC_State = SOC_DISCHARGING;
+  } else {
+    *socData.SOC_State = SOC_IDLE;
+  }
 }
 
-void SOC_FSMStateActions (SOCTaskData socData){
+public void SOC_FSMStateActions (SOCTaskData socData){
   switch (*(socData.SOC_State)) { // state actions\
-    case PC_START:
-    
-    case PC_OPEN:
-
+    case SOC_START:
       break;
-    case PC_CLOSE:
-
+    case SOC_CHARGING:
+      *socData.stateOfCharge = adjustSOC(socData, *socData.chargingCurrent);
+      *socData.batteryCapacity = *socData.batteryCapacity - (*socData.chargingCurrent * SOC_TIME_INTERVAL);
       break;
-    case PC_JUST_CLOSED:
-
+    case SOC_DISCHARGING:
+      *socData.stateOfCharge = adjustSOC(socData, *socData.dischargingCurrent);
+      *socData.batteryCapacity = *socData.batteryCapacity + (*socData.dischargingCurrent * SOC_TIME_INTERVAL);
+      break;
+    case SOC_IDLE:
       break;
     default:
       break;
   } // state actions
-
-  }
 }
