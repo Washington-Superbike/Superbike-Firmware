faa90177a114fe48ef42bd328b0c1d14805d649c
Atharva Mattam <mattam.lampu@gmail.com>
2022-11-24 22:29:35 -0800

    Blink

---
+++ Pico/Blink/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+int main() {
+
+    const uint led_pin = 25;
+    uint count = 0;
+    // Initialize LED pin
+    gpio_init(led_pin);
+    gpio_set_dir(led_pin, GPIO_OUT);
+
+    // Initialize chosen serial port
+    stdio_init_all();
+
+    // Loop forever
+    while (true & count < 10) {
+
+        // Blink LED
+        printf("Blinking!\r\n");
+        gpio_put(led_pin, true);
+        sleep_ms(1000);
+        gpio_put(led_pin, false);
+        sleep_ms(1000);
+        count = count + 1;
+    }
+    gpio_put(led_pin, false);
+}
