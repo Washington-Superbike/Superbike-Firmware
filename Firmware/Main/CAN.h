045d415844d20e4ab8b4ffbb72068ac12b119db2
Joseph Suleiman <118699379+josulei@users.noreply.github.com>
2025-03-15 01:34:43 -0700

    Update CAN.h
    small addition.

--- Firmware/Main/CAN.h
+++ Firmware/Main/CAN.h
@@ -10,6 +10,8 @@
 
 typedef uint8_t byte;
 
+#define ERROR_LED_PIN 3
+
 /// Uses the configMINIMAL_STACK_SIZE variable in Main.h to add up to the stack size used for the canTask()
 #define CAN_TASK_STACK_SIZE configMINIMAL_STACK_SIZE + 1000
 
