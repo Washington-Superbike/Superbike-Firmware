d9cd730fc0507bf7c94ec6df85f84bafd37553b9
Joseph Suleiman <118699379+josulei@users.noreply.github.com>
2025-03-15 02:15:55 -0700

    Update CAN.h
    fix test.

--- Firmware/Main/CAN.h
+++ Firmware/Main/CAN.h
@@ -14,7 +14,7 @@ typedef uint8_t byte;
 
  bool ledState;
  unsigned long previousMillis;
-int testVar = 0;
+
 
 /// Uses the configMINIMAL_STACK_SIZE variable in Main.h to add up to the stack size used for the canTask()
 #define CAN_TASK_STACK_SIZE configMINIMAL_STACK_SIZE + 1000
