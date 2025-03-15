cca8958898f7ee357d4154a1f9889392f593c554
Joseph Suleiman <118699379+josulei@users.noreply.github.com>
2025-03-15 01:49:37 -0700

    Update CAN.h
    Moving variables to global so other file can access them.

--- Firmware/Main/CAN.h
+++ Firmware/Main/CAN.h
@@ -12,6 +12,9 @@ typedef uint8_t byte;
 
 #define ERROR_LED_PIN 3
 
+bool ledState = LOW; //Initial LED state set as low (0 or false)
+unsigned long previousMillis = 0; // Stores the last time the LED was toggled
+
 /// Uses the configMINIMAL_STACK_SIZE variable in Main.h to add up to the stack size used for the canTask()
 #define CAN_TASK_STACK_SIZE configMINIMAL_STACK_SIZE + 1000
 
